92be677fce0e5c3e83fb0931a8b39b669498f596
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-09 12:48:51 +0200

    better unicode examples

--- examples/unicode/unicode_problems.py
+++ examples/unicode/unicode_problems.py
@@ -3,21 +3,33 @@
 import sys
 import codecs
 
-# print(sys.stdout.encoding)
-# sys.exit(0)
+assert sys.stdout.encoding=='UTF-8'
 
-# you need these two lines for redirection to work
+# To make redirection work you need one of the following:
 if sys.stdout.encoding is None:
     sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 # or this:
 # $ export PYTHONIOENCODING=UTF-8
 # in the environment
 
+# lets show how to decode unicode data
 data = u'\xe9'
-t = bytes(data)
+assert type(data) == unicode
+try:
+    bytes(data)
+except UnicodeEncodeError:
+    pass
+
+# this is the right way to decode data (the type of the output is 'str'
+# which is python 2 version of 'bytes')
+t = data.encode('utf-8')
+assert type(t) == str
+
+# lets show how to print unicode data
 print(data)
+# this will not work because the encoder will be ascii
 try:
     print('this is data {}'.format(data))
 except UnicodeEncodeError:
-    print("yes, got exception")
+    pass
 print(u'this works ok {}'.format(data))
