ed132e6a57e9f6a87279d36d28bc2368f955e2b5
Mark Veltzer <mark.veltzer@gmail.com>
2018-02-04 15:20:29 +0200

    another enum example

--- examples/short/enum/basic.py
+++ examples/short/enum/basic.py
@@ -16,3 +16,11 @@ print(type(SampleEnum.one))
 # convert an int to a name
 print(SampleEnum(2).name)
 print(SampleEnum.one.value)
+# print all strings of the enum
+print(SampleEnum.__members__.keys())
+# prnit all enum values
+print(SampleEnum.__members__.values())
+# print all values as ints
+print([x.value for x in SampleEnum.__members__.values()])
+# find the max value
+print(max(x.value for x in SampleEnum))
