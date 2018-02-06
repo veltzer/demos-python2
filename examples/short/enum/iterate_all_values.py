b896364126c1ac0aa84744102899c9810ef08070
Mark Veltzer <mark.veltzer@gmail.com>
2018-02-06 12:45:40 +0200

    added notes

--- examples/short/enum/iterate_all_values.py
+++ examples/short/enum/iterate_all_values.py
@@ -12,7 +12,14 @@ class SampleEnum(enum.Enum):
     two = 2
     three = 3
 
+# This works but the problem is that pycharm complains about it
 for x in SampleEnum:
+    print('actual enum value is [{}]'.format(x))
+    print('int is [{}]'.format(x.value))
+    print('name is [{}]'.format(x.name))
+
+# pycharm does not complain about this one...
+for x in SampleEnum.__members__.values():
     print('actual enum value is [{}]'.format(x))
     print('int is [{}]'.format(x.value))
     print('name is [{}]'.format(x.name))
