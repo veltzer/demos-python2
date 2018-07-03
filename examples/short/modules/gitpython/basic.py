ae418463bfd7cf9557fb48287cab5a2896d8460a
Mark Veltzer <mark.veltzer@gmail.com>
2018-07-03 18:22:52 +0300

    a git example

---
+++ examples/short/modules/gitpython/basic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""
+List all remote branches of a git repo
+
+References:
+- https://stackoverflow.com/questions/2473035/checkout-or-list-remote-branches-in-gitpython
+"""
+
+from git import Repo
+
+repo_path = "git@bitbucket.org:shopymate/shopymate.git"
+r = Repo(repo_path)
+repo_heads = r.heads
+repo_heads_names = [h.name for h in repo_heads]
